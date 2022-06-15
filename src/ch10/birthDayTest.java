7ddfcdeb6d2ac05fc92616301c96f44f2296ae00
kkwondev <kkwoncokr@gmail.com>
2022-06-15 22:01:25 +0900

    ch10

---
+++ src/ch10/birthDayTest.java
@@ -0,0 +1,11 @@
+package ch10;
+
+public class birthDayTest {
+    public static void main(String[] args) {
+        BirthDay date = new BirthDay();
+        date.setYear(1996);
+        date.setMonth(5);
+        date.setDay(30);
+        date.showDate();
+    }
+}
